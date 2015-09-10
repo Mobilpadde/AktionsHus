c8398e0a7549e729d5dbc310dae42ee0646e9bbb
Martin <Mart7972@edu.eal.dk>
2015-09-10 10:01:53 +0200

    Tilføjet nogle autktionsvare
    WEEE ;D

---
+++ Socketeer/Server/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Product
+    {
+        public string Name { get; set; }
+
+        public string ProductType { get; set; }
+
+        public double MinPrice { get; set; }
+
+        public double HighestPrice { get; set; }
+
+        public string Id { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public Product(string name, string productType, double minPrice, double highestPrice, string id)
+        {
+            Name = name;
+            ProductType = productType;
+            MinPrice = minPrice;
+            HighestPrice = highestPrice;
+            Id = id;
+
+            SetTime();
+        }
+
+        public void SetTime()
+        {
+            DateTime d = DateTime.Now;
+            d.AddSeconds(18);
+            Time = d;
+        }
+    }
+}
--- Socketeer/Server/Program.cs
+++ Socketeer/Server/Program.cs
@@ -11,7 +11,17 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>
+            {
+                new Product("Arne Jacobsen", "Stol", 40000, double.MinValue, Guid.NewGuid().ToString()),
+                new Product("Bo Henriksen", "Vase", 500, double.MinValue, Guid.NewGuid().ToString()),
+                new Product("Italinano", "Komode", 2100, double.MinValue, Guid.NewGuid().ToString()),
+                new Product("Claude Monet", "Maleri", 400000, double.MinValue, Guid.NewGuid().ToString()),
+
+            };
+            
             Socketeer.Server sq85 = new Socketeer.Server(5000);
+
         }
     }
 }
